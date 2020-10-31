09adf6fa48018be3c4a3a14858752b4b1667d3e8
Saksham Tiwari <contact@tsaksham.com>
2020-10-30 22:44:09 -0400

    Solve me first commit.
    Solve me first commit.

---
+++ 1 - Solve Me First/SolveMeFirst.java
@@ -0,0 +1,25 @@
+/**
+ * Solve Me First
+ *
+ * Time complexity:
+ * Worst case: Θ(1)
+ * Best case: Θ(1)
+ *
+ * Space complexity:
+ * Worst case: Θ(1)
+ * Best case: Θ(1)
+ *
+ * @author Saksham Tiwari
+ */
+
+public class SolveMeFirst {
+    /**
+     * Function that adds two numbers.
+     * @param a Integer one.
+     * @param b Integer two.
+     * @return Sum of the integers a and b.
+     */
+    static int solveMeFirst(final int a, final int b) {
+        return a + b;
+    }
+}
