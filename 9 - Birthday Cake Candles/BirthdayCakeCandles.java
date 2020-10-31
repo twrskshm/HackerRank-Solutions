33a547ffb83c37646183904599ccb755c2aa48fb
Saksham Tiwari <60761912+twrskshm@users.noreply.github.com>
2020-10-31 03:21:48 -0400

    Update BirthdayCakeCandles.java

--- 9 - Birthday Cake Candles/BirthdayCakeCandles.java
+++ 9 - Birthday Cake Candles/BirthdayCakeCandles.java
@@ -16,9 +16,9 @@ import java.util.List;
 
 public class BirthdayCakeCandles {
     /**
-     * Function that calculates the total number of candles that can be blown
+     * Function that calculates the total number of candles that can be blown.
      * @param candles Integer array that containing heights of all the candles.
-     * @return Returns total number of candles that can be blown
+     * @return Returns total number of candles that can be blown.
      */
     public static int birthdayCakeCandles(List<Integer> candles) {
         int largestElement = Integer.MIN_VALUE;
